48108b01bd38b42c277680d9964d6f3743053db6
Amitee Weiner <amitweiner@google.com>
2021-12-29 20:41:32 +0200

    fix explore header expand issue

--- frontend/src/cmps/AppHeader/AppHeader.jsx
+++ frontend/src/cmps/AppHeader/AppHeader.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { loadScript } from '../../services/util.service';
-import { setSearchExpand, setGoogleScriptLoad } from '../../store/actions/appActions';
+import { setHomeTop, setSearchExpand, setGoogleScriptLoad } from '../../store/actions/appActions';
 import { SearchBar } from './SearchBar/SearchBar';
 import { UserMenuBtn } from './UserMenuBtn';
 import WhiteLogo from '../../assets/imgs/logo-white.svg';
@@ -17,13 +17,19 @@ const scriptUrl = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_KEY}&lib
 export const AppHeader = () => {
     const dispatch = useDispatch();
     const { pathname } = useLocation();
-    const layoutType = pathname.includes('/stay/') ? " stay" : (pathname.includes('/explore/') ? " explore" : "");
+    const pageType = pathname.includes('/stay') ? " stay" : (pathname.includes('/explore') ? " explore" : "");
     const { isHomeTop, isSearchExpand, isGoogleScriptLoaded } = useSelector(state => state.appModule);
 
     useEffect(() => {
-        if (!isGoogleScriptLoaded) loadScript('google-maps',scriptUrl, {
-            async: true, 
-            // defer: true,
+        if (pageType) {
+            dispatch(setHomeTop(false));
+            dispatch(setSearchExpand(false));
+        }
+    }, [pageType, isHomeTop]);
+
+    useEffect(() => {
+        if (!isGoogleScriptLoaded) loadScript('google-maps', scriptUrl, {
+            async: true,
             callback: () => { dispatch(setGoogleScriptLoad(true)); }
         });
     }, [isGoogleScriptLoaded]);
@@ -31,24 +37,25 @@ export const AppHeader = () => {
     useEffect(() => {
         if (isSearchExpand && !isHomeTop) {
             window.addEventListener('keydown', isEsc);
-            console.log('keydown listener on in header');
         }
         return () => {
             window.removeEventListener('keydown', isEsc);
-            console.log('keydown listener off in header');
         };
     }, [isSearchExpand]);
 
     const isEsc = (ev) => {
-        console.log('keydown in hdr');
         if (ev.key === 'Escape') dispatch(setSearchExpand(false));
     };
 
+    const isScrollTop = () => {
+        if (!pageType) window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
-        <header className={`content-wrapper${layoutType}${isHomeTop ? " home-top" : ""}${isSearchExpand ? " expanded" : ""}`}>
+        <header className={`content-wrapper${pageType}${isHomeTop ? " home-top" : ""}${isSearchExpand ? " expanded" : ""}`}>
             <div className="header-content flex column">
                 <div className="top flex align-center">
-                    <Link to="/" className="logo-wrapper flex align-center">
+                    <Link to="/" className="logo-wrapper flex align-center" onClick={isScrollTop}>
                         <img className="logo" src={isHomeTop ? WhiteLogo : RedLogo} alt="" />
                         <h1 className="name">HomeAway</h1>
                     </Link>
